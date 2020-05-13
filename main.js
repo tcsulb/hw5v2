386a9905132a238bab7d3ad89190583f7d087d53
Tianna <tianna.hydro@csulb.edu>
2020-05-12 18:47:13 -0700

    done?

--- main.js
+++ main.js
@@ -1,45 +1,112 @@
-let formElement = document.querySelector("form");
-let btn = document.querySelector("#submit");
+
+const btn = document.querySelector("#submit");
+const n1  = parseInt(document.getElementById('num1').value);
+const n2 = parseInt(document.getElementById('num2').value);
 let output1 = document.querySelector("#output1").innerHTML="";
 let output2 = document.querySelector("#output2").innerHTML="";
 
 
-formElement.addEventListener("click", (e) => {
-    e.preventDefault();
-    const n1 = formElement.querySelector("#num1").value;
-    const n2 = formElement.querySelector("#num2").value;
-// first check if inputs are between 2 and 100
-    if (n1 >= 2 && n1 <= 100) {
-        if (n2 >= 2 && n2 <= 100 ) {
-        // check which is bigger
-            if (n1<n2) {
-                //output2
-                result = getPrimes(n1,n2);
+btn.addEventListener("click", validateInputs(n1, n2));
+
+
+function validateInputs(first, second) {
+    let check = true;
+    if (first < 2 || second < 2) {
+        //check = false;
+        result1 = "";
+        result2 = "Invalid input, please try again";
+        break;
+    } 
+    else if (first > 100 || second > 100) {
+        result1 = "";
+        result2 = "Invalid input, please try again";
+        break;
+        //check = false;
+    }
+    else if (checkPrime(start) === false || checkPrime(second) === false) {
+        result1="";
+        result2 = "Invalid input, please try again";
+        break;
+    }
+    else {
+        if (first < second) {
+            primes = getPrimes(first, second);
+            
+        }
+        else {
+            primes = getPrimes(second, first);
+            
+        }
+        result1 = (`There are ${primes.length} prime numbers`);
+        result2 = (primes + ',');
+    }
+    let output1 = document.querySelector("#output1").innerHTML = result1;
+    let output2 = document.querySelector("#output2").innerHTML = result2;
+}
+
+function checkPrime(num) {
+    if (num === 2) {
+        return true;
+    }
+    else if (num > 1) {
+        for (let i = 2; i < num; i++) {
+            if (num % i !== 0) {
+                return true;
+            }
+            else if (num === i * i) {
+                return false
             }
             else {
-                result = getPrimes(n2,n1);
+                return false;
             }
         }
-    } 
-    else {
-        //output2
-        result = "Invalid input, please try again";
     }
-    return result;
+    else {
+        return false;
+    }
+}
+
+function getPrimes(start, stop) {
+    let primes = [];
+    for (let i = 2; i <= stop; i++) {
+        let isPrime = true;
+        for (var j = 0; j < primes.length; j++) {
+            let d = primes[j];
+            if (i % d === 0) {
+                //it is divisible by another prime, so it's not prime
+                isPrime = false;
+                break;
+            }
+            //you don't need to check primes bigger than sqrt(i)
+            if (d * d > i)
+                break;
+        }
+        if (isPrime){
+            primes.push(i);
+        }            
+    }
+    return primes;
     
-});
+
+    
+    /* document.getElementById('output1').innerHTML = (`There are ${primes.length} prime numbers`);
+    document.querySelector("#output2").innerHTML = (primes + ','); */
+    
+};
 
 
-function getPrimes(start, end) {
-    let primes = new Array();
-    while (start <= end) {
+
+
+
+
+/*  while (start <= end) {
         if (checkPrime(start) === true && checkPrime(end) === true) {
             primes[primes.length] = start;
         }
         start = start + 1;  
-    }
+    } */
 
-    if (primes.length === 0) {
+   /*  if (count === 0) {
         //output2
         result2 = "Invalid input, please try again";
     }  
@@ -51,18 +118,11 @@ function getPrimes(start, end) {
     document.querySelector('#output2').innerHTML= result2;
     document.querySelector('#output1').innerHTML = result1;
 }
+ */
 
-function checkPrime(num) {
-    let check = true;
-    for (let i = 2; i <= Math.ceil(num / 2); i++) {
-        if ((num % i) === 0) {
-            check = false;
-            break;
-        }
-    }
-    return check;
-}
 
+
+/*
 function listPrimes(vals) {
     let text = "<h2>Prime Numbers</h2>";
     for (i = 0; i < vals.length; i++) {
@@ -70,63 +130,6 @@ function listPrimes(vals) {
     }
     return text;
     
-}
-
+} */
 
     
-//     //clear the result div
-//     document.getElementById("result").innerHTML = '';
-
-//     //loop till i equals to end
-//     for (i = start; i <= end; i++) {
-//         c = 0;
-//         for (j = 1; j <= i; j++) {
-//             // % modules will give the reminder value, so if the reminder is 0 then it is divisible
-//             if (i % j === 0) {
-//                 //increment the value of c
-//                 c++;
-//             }
-//         }
-
-//         //if the value of c is 2 then it is a prime number
-//         //because a prime number should be exactly divisible by 2 times only (itself and 1)
-//         if (c == 2) {
-//             document.getElementById("result").insertAdjacentHTML('beforeend', i + '<br>');
-//         }
-//     }
-// }
-    
-// /* function calcPrimeNumber() {
-
-//     
-//  */
-
-// //check if inputs are prime
-// /* function primes() {
-//     const num1 = document.getElementById("num1");
-//     const num2 = document.getElementById("num2");
-
-//     //find start and end 
-//     if (num1 < num2) {
-//         let start = num1;
-//         let end = num2;
-//         //check 
-//     }
-//     else {
-//         let start = num2;
-//         let end = num1;
-
-//     }
-
-//     if (start >= 2 && end >= 2) {
-//         if (start < 100 && end < 100) {
-//             if (start % 2 === 0)
-//         }
-//     } 
-//     else {
-//         result1 = "Invalid input, please try again";
-//     } 
-//     else if ()
-//     for(start=)
-//         result1 = "Invalid input, please try again";
-// } */
